3fb9ddee071e79bcacffcf4769d4f5764abb1cc6
Tsiky98 <randriamiandrisoatsikinjanahar@gmail.com>
2023-09-11 19:19:18 +0300

    second commit

--- JS/app.js
+++ JS/app.js
@@ -8,12 +8,12 @@ const nav = document.querySelector ("nav")
 function is_Open (nav_el,class_name,btn_el,current_img,new_img) {
     nav_el.classList.toggle (class_name)        
         if (btn_el.src.includes(current_img)) {
-            btn_el.src = "../images/"+ new_img;
+            btn_el.src = "./images/"+ new_img;
             nav.style.background = "#f1f1f1"
             fondu.classList.add (class_name);
             
         } else {
-            btn_el.src = "../images/"+ current_img;
+            btn_el.src = "./images/"+ current_img;
             nav.style.background = "#fff"
             fondu.classList.remove (class_name);
        }
@@ -30,7 +30,7 @@ btnNav.addEventListener ("click",() => {
 fondu.addEventListener ("click", () =>  {
     fondu.classList.remove ("active");
     listNav.classList.toggle ("active")
-    img_Btn.src = "../images/icon-hamburger.svg";
+    img_Btn.src = "./images/icon-hamburger.svg";
     nav.style.background = "#fff"
 })
 
